231086021c658878c57910b4463d1491f304abac
BuddhikaSadun <sadunjayamaha917@gmail.com>
2025-07-04 09:55:42 +0530

    Updated name field commit

--- backend/modules/feedback.js
+++ backend/modules/feedback.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 
 const feedbackSchema = mongoose.Schema({
-	firstName: {
-		type: String,
-	},
-	lastName: {
-		type: String,
-	},
-	email: {
-		type: String,
-	},
-	message: {
-		type: String,
-	},
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  message: {
+    type: String,
+  },
 });
 
 const Feedback = mongoose.model("Feedback", feedbackSchema);
